003e0d2de1c49433f349191ad813fb734d7db719
Andrei V <vasiluta.ai@gmail.com>
2020-11-23 22:07:07 +0200

    FIRST + FOLLOW

---
+++ LFTC - LABORATOR 4/Grammar.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFTC___LABORATOR_4
+{
+    public class Production
+    {
+        public string LeftHandSide { get; set; }
+        public List<string> RightHandSide { get; set; }
+
+        public Production(string leftHandSide, List<string> rightHandSide)
+        {
+            this.LeftHandSide = leftHandSide;
+            this.RightHandSide = rightHandSide;
+        }
+    }
+
+    public class Grammar
+    {
+        public string StartingSymbol { get; set; }
+        public List<string> NonTerminals { get; set; }
+        public List<string> Terminals { get; set; }
+        public List<Production> Productions { get; set; }
+
+        public Grammar(string startingSymbol, List<string> nonTerminals, List<string> terminals, List<Production> productions)
+        {
+            this.StartingSymbol = startingSymbol;
+            this.NonTerminals = nonTerminals;
+            this.Terminals = terminals;
+            this.Productions = productions;
+        }
+
+        public Dictionary<string, List<string>> GetFirstUs()
+        {
+            var firstSet = new Dictionary<string, List<string>>();
+
+            foreach (var terminal in this.Terminals)
+            {
+                firstSet.Add(terminal, new List<string> { terminal });
+            }
+
+            firstSet = this.ExecuteFirstIteration(firstSet, out var trimmedProductions);
+
+            for (int i = 0; i < 2; i++)
+            {
+                // foreach remaining production
+                trimmedProductions.ForEach(p =>
+                {
+                    var lastIndex = p.RightHandSide.Count() - 1;
+                    var nonTerminal = p.LeftHandSide;
+
+                    foreach (var element in p.RightHandSide)
+                    {
+                        var firstSetOfElement = firstSet[element];
+
+                        //
+                        if (!firstSetOfElement.Any())
+                        {
+                            break;
+                        }
+
+                        if (!firstSetOfElement.Contains("epsilon"))
+                        {
+
+                            firstSetOfElement.ForEach(f =>
+                            {
+                                if (!firstSet[nonTerminal].Contains(f)) firstSet[nonTerminal].Add(f);
+                            });
+
+                            break;
+                        }
+
+                        if (p.RightHandSide.IndexOf(element) == lastIndex)
+                        {
+                            if (!firstSet[nonTerminal].Contains("epsilon")) firstSet[nonTerminal].Add("epsilon");
+                        }
+                    }
+                });
+            }
+
+            return firstSet;
+        }
+
+        private Dictionary<string, List<string>> ExecuteFirstIteration(Dictionary<string, List<string>> firstSet, out List<Production> trimmedProductions)
+        {
+            var resultedList = new List<Production>();
+
+            this.Productions.ForEach(p =>
+            {
+                var firstOfRightHandSide = p.RightHandSide.First();
+                var nonTerminal = p.LeftHandSide;
+
+                // Create entry for every non terminal.
+                if (!firstSet.ContainsKey(nonTerminal))
+                {
+                    firstSet.Add(nonTerminal, new List<string>());
+                }
+
+                // Check whether the first symbol is terminal or epsilon.
+                if (Terminals.Contains(firstOfRightHandSide) || firstOfRightHandSide.Equals("epsilon"))
+                {
+                    // Check for duplicate symbol.
+                    if (!firstSet[nonTerminal].Contains(firstOfRightHandSide))
+                    {
+                        firstSet[nonTerminal].Add(firstOfRightHandSide);
+                    }
+                }
+                else
+                {
+                    resultedList.Add(p);
+                    Console.WriteLine(String.Join(" ", p.RightHandSide));
+                }
+            });
+
+            trimmedProductions = resultedList;
+            return firstSet;
+        }
+
+        public Dictionary<string, List<string>> GetFollowUs()
+        {
+            var firstSet = this.GetFirstUs();
+
+            Dictionary<string, List<string>> followSet = new Dictionary<string, List<string>>
+            {
+                {StartingSymbol, new List<string>{"epsilon"}}
+            };
+
+            foreach (var nonTerminal in this.NonTerminals)
+            {
+                if (!followSet.ContainsKey(nonTerminal))
+                {
+                    followSet.Add(nonTerminal, new List<string>());
+                }
+
+                foreach (var production in Productions)
+                {
+
+                    if (production.RightHandSide.Contains(nonTerminal))
+                    {
+                        List<string> toBeAdded = new List<string>();
+
+                        var indexOfNonTerminalInProduction = production.RightHandSide.IndexOf(nonTerminal);
+                        if (indexOfNonTerminalInProduction < production.RightHandSide.Count - 1)
+                        {
+
+                            var beta = production.RightHandSide[indexOfNonTerminalInProduction + 1];
+                            if (beta != "epsilon")
+                            {
+
+                                if (!firstSet[beta].Contains("epsilon"))
+                                {
+                                    firstSet.TryGetValue(beta, out var set);
+                                    if (set != null)
+                                    {
+                                        toBeAdded = set;
+                                    }
+                                }
+                                else
+                                {
+                                    toBeAdded = firstSet[beta].Select(s => s).Where(s => s != "epsilon").ToList();
+
+                                    if (followSet.ContainsKey(production.LeftHandSide))
+                                    {
+                                        toBeAdded.AddRange(followSet[production.LeftHandSide]);
+                                    }
+                                }
+                            }
+
+
+
+                        }
+                        else if (followSet.ContainsKey(production.LeftHandSide))
+                        {
+                            toBeAdded = followSet[production.LeftHandSide];
+                        }
+
+                        foreach (var element in toBeAdded)
+                        {
+                            if (!followSet[nonTerminal].Contains(element))
+                            {
+                                followSet[nonTerminal].Add(element);
+                            }
+                        }
+                    }
+                }
+            }
+
+            followSet[StartingSymbol].Reverse();
+            followSet[StartingSymbol].Add("$");
+            followSet[StartingSymbol].Reverse();
+            return followSet;
+        }
+
+
+        public Production GetProductionContainingTerminal(string terminal)
+        {
+            foreach (var production in this.Productions)
+            {
+                if (production.RightHandSide.Contains(terminal))
+                {
+                    Console.WriteLine(production.LeftHandSide + " -> " + String.Join(" ", production.RightHandSide));
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LFTC - LABORATOR 4/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LFTC___LABORATOR_4
+{
+    class Program
+    {
+        static Grammar grammar;
+        static string inputSequence;
+
+        static void MenuOption(int opt)
+        {
+            string grammarFile = "";
+
+            switch (opt)
+            {
+                case 1:
+                    grammarFile = "basicGrammar.txt";
+                    string inputSequenceFile = "inputSequence.txt";
+
+                    ReadGrammarFromFile(grammarFile);
+                    ReadInputSequenceFromFile(inputSequenceFile);
+                    break;
+                case 2:
+                    grammarFile = "minilanguageGrammar.txt";
+                    string programInternalFormFile = "programInternalForm.txt";
+
+                    ReadGrammarFromFile(grammarFile);
+                    ReadProgramInternalFormFromFile(programInternalFormFile);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void ReadGrammarFromFile(string grammarFile)
+        {
+            string startingSymbol = "";
+            List<string> nonTerminals = new List<string>();
+            List<string> terminals = new List<string>();
+            List<Production> productions = new List<Production>();
+
+            string line;
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(grammarFile);
+                int k = 0;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (k == 0 && line != "")
+                        startingSymbol = line.Trim();
+
+                    if (k == 1 && line != "")
+                    {
+                        string[] strings = line.Split(',', ' ');
+                        nonTerminals.AddRange(strings.ToList<string>());
+                    }
+
+                    if (k == 2 && line != "")
+                    {
+                        string[] strings = line.Split(',', ' ');
+                        terminals.AddRange(strings.ToList<string>());
+                    }
+
+                    if (k > 2 & line != "")
+                    {
+                        string[] strings = line.Split('-');
+                        string[] results = strings[1].Split(' ');
+                        Production p = new Production(strings[0], results.ToList<string>());
+                        productions.Add(p);
+                    }
+
+                    if (line == "")
+                        k++;
+                }
+
+                grammar = new Grammar(startingSymbol, nonTerminals, terminals, productions);
+
+                file.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " " + e.StackTrace);
+            }
+        }
+
+        static void ReadInputSequenceFromFile(string inputSequenceFile)
+        {
+            string line;
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(inputSequenceFile);
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    inputSequence = line;
+                }
+
+                file.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " " + e.StackTrace);
+            }
+        }
+
+        static void ReadProgramInternalFormFromFile(string programInternalFormFile)
+        {
+            string line;
+            inputSequence = "";
+
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(programInternalFormFile);
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    int code = Convert.ToInt32(line.Split(' ')[1]);
+
+                    inputSequence = inputSequence + code + " ";
+                }
+
+                file.Close();
+
+                Console.WriteLine(inputSequence);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " " + e.StackTrace);
+            }
+        }
+
+        static void WriteToConsole()
+        {
+            string startingSymbol = grammar.StartingSymbol;
+            List<string> nonTerminals = grammar.NonTerminals;
+            List<string> terminals = grammar.Terminals;
+            List<Production> productions = grammar.Productions;
+
+            Console.WriteLine("Initial Grammar:");
+            Console.Write($"Starting symbol: {startingSymbol}");
+
+            Console.WriteLine();
+
+            Console.Write($"Non terminals: ");
+            foreach (string s in nonTerminals)
+                Console.Write(s + " ");
+
+            Console.WriteLine();
+
+            Console.Write("Terminals: ");
+            foreach (string s in terminals)
+                Console.Write(s + " ");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Productions: ");
+            foreach (Production p in productions)
+            {
+                Console.Write(p.LeftHandSide + "-");
+
+                for (int i = 0; i < p.RightHandSide.Count; i++)
+                {
+                    if (i == p.RightHandSide.Count - 1)
+                        Console.Write(p.RightHandSide[i]);
+                    else
+                        Console.Write(p.RightHandSide[i] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Generated First Set: ");
+
+            Dictionary<string, List<string>> firstSet = grammar.GetFirstUs();
+
+            foreach (KeyValuePair<string, List<string>> kv in firstSet)
+            {
+                Console.Write(kv.Key + " - ");
+                foreach (string v in kv.Value)
+                    Console.Write(v + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Generated Follow Set: ");
+
+            Dictionary<string, List<string>> followSet = grammar.GetFollowUs();
+
+            foreach (KeyValuePair<string, List<string>> kv in followSet)
+            {
+                Console.Write(kv.Key + " - ");
+                foreach (string v in kv.Value)
+                    Console.Write(v + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Terminal to show productions of : ");
+            grammar.GetProductionContainingTerminal(Console.ReadLine());
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string option;
+
+            Console.WriteLine("Options: ");
+            Console.WriteLine("1 - Step 1 - Basic Grammar");
+            Console.WriteLine("2 - Step 2 - Minilanguage Grammar");
+            Console.WriteLine();
+            Console.Write("Select an option: ");
+
+            option = Console.ReadLine();
+            Console.WriteLine();
+
+            MenuOption(Int32.Parse(option));
+            WriteToConsole();
+        }
+    }
+}
